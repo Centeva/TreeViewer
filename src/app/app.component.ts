d86030da255fe3a852ff29f91533f2be80d74b0d
Devyn Cyphers <devyn.cyphers@centeva.com>
2018-07-25 10:49:52 -0600

    check in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as domtoimage from 'dom-to-image';
+import * as fs from 'file-saver';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor() {
+    setTimeout(() => {
+    const node = document.getElementsByClassName('viewer')[0];
+
+    domtoimage.toBlob(node).then(function (blob) {
+        fs.saveAs(blob, 'my-node.png');
+    });
+  }, 10000);
+  }
 }
